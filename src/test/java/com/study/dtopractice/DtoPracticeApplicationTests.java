658dc12ce072c9067768d6db17122fa3150a2ff3
MinYoonhong <dbsghd@badmintonfriends.co.kr>
2026-01-05 20:29:46 +0900

    first commit

---
+++ src/test/java/com/study/dtopractice/DtoPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.dtopractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DtoPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
